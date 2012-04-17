6852c2746edbe4243420682dba6b5844b5637689
moti.ben.ari <moti.ben.ari@c6fa14eb-e534-0410-a1bc-770b10e7a209>
2012-04-17 10:21:29 +0000

    Removing unneeded files from LearnSAT directory

--- trunk/learnsat/z.bat
+++
@@ -1,6 +0,0 @@
-set back=learnsat
-7z u -r -tzip c:\%back%\%back%.zip *.pro copyright.txt learnsat.tex learnsat-release.html graph.png graph.eps
-copy %back%.zip \zip
-copy %back%.zip n:\zip
-set back=
-pause
--- trunk/learnsat/zdist.bat
+++
@@ -1,2 +0,0 @@
-7z u -r -tzip c:\learnsat\learnsat-%1.zip *.pro learnsat.tex learnsat.pdf copyright.txt gpl.txt learnsat-release.html graph.png graph.eps
-pause
